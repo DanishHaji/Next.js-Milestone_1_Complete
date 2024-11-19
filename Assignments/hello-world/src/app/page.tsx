f52007fa6875b04b558fc6465da9683eb71f14db
DanishHaji <danish.haji111@gmail.com>
2024-11-19 16:53:46 +0500

    Final updates

---
+++ Assignments/hello-world/src/app/page.tsx
@@ -0,0 +1,5 @@
+export default function Home () {
+  return (
+    <div>Hello World !!! Welcome to Next.js</div>
+  )
+}
---
+++ Project/simple-web/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home () {
+  return(
+    <div className="container">
+      <h1>Hello, World! Welcome to my Next.js Page!</h1>
+      <p>This is a simple, personalized greeting with basic styling.</p>
+    </div>
+  )
+}
